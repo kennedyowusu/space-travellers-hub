195659728471ec00b396e536c5375336676d0b67
elarhadu <elarhadu@gmail.com>
2023-01-31 10:01:40 +0000

    Create website header with empty nav - Setup

---
+++ src/Components/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import planet from '../assets/images/planet.png';
+import './Navigation.css';
+
+export default function Navigation() {
+  return (
+    <nav className="nav-menu">
+      <div className="logo-title">
+        <img src={planet} alt="planet-logo" className="app-logo" />
+        <h1>Space Travelers&apos; Hub</h1>
+      </div>
+      <div>
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/">Rockets</NavLink>
+          </li>
+          <li>
+            <NavLink to="/missions">Missions</NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile">My Profile</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
